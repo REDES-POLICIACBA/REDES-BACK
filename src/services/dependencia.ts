7dfd25d15a8b592792b174e9b17e3767d5f062cd
MatiasTejerina07 <matiastejerina94@gmail.com>
2024-07-05 16:21:09 -0300

    dependencia update

--- src/services/dependencia.ts
+++ src/services/dependencia.ts
@@ -1,5 +1,7 @@
 import type Dependencia from '../interfaces/Dependencia'
 import type { Model } from 'mongoose'
+import type { ParamsDictionary } from 'express-serve-static-core'
+
 class DependenciaService {
   public DependenciaModel: Model<Dependencia>
   constructor(DependenciaModel: Model<Dependencia>) {
@@ -13,6 +15,20 @@ class DependenciaService {
     } catch (error) {
       throw new Error(
         `Ha ocurrido un error al intentar crear la dependencia: ${error}`,
+      )
+    }
+  }
+  async update(dependencia: Dependencia, params: ParamsDictionary) {
+    try {
+      const dependenciaUpdated = await this.DependenciaModel.findByIdAndUpdate(
+        { _id: params },
+        dependencia,
+        { new: true },
+      )
+      return dependenciaUpdated
+    } catch (error) {
+      throw new Error(
+        `Ha ocurrido un error al intentar actualizar la dependencia: ${error}`,
       )
     }
   }
