b307e07dc23063458be20aa1a786044b36734731
MatiasTejerina07 <matiastejerina94@gmail.com>
2024-07-11 16:13:45 -0300

    user update

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -4,23 +4,24 @@ import accountExist from '../middlewares/accountExist'
 import passwordIsOk from '../middlewares/passIsOk'
 import isVerified from '../middlewares/isVerified'
 import passport from '../middlewares/passport'
+import accountIsExist from '../middlewares/accountIsExist'
 const router = Router()
 
-router.post('/', userController.createUser)
+router.post('/', accountIsExist, userController.createUser)
 router.put('/:id', userController.updateUser)
 router.delete('/:id', userController.deleteUser)
 router.post('/recoverypassword', userController.recoveryPassword)
 router.post(
-  '/signin',
-  accountExist,
-  passwordIsOk,
-  isVerified,
-  userController.signIn,
+    '/signin',
+    accountExist,
+    passwordIsOk,
+    isVerified,
+    userController.signIn,
 )
 
 router.post(
-  '/signin/token',
-  passport.authenticate('jwt', { session: false }),
-  userController.signInToken,
+    '/signin/token',
+    passport.authenticate('jwt', { session: false }),
+    userController.signInToken,
 )
 export default router
