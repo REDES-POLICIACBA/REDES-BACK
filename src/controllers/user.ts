c779d3225f0ea36a4f50989b62837df4fd26bbda
Mat <matiastejerina94@gmail.com>
2024-07-11 16:37:48 -0300

    signin token recuperado ok

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -8,59 +8,61 @@ import type CustomError from '../interfaces/CustomError'
 const services = new UserServices(User)
 
 export const userController = {
-  async createUser(req: Request, res: Response) {
-    try {
-      await services.createUser(req.body)
-      response.isOk(res, 201, 'Usuario creado correctamente')
-    } catch (error) {
-      response.isError(res, 500, error as CustomError, 'user')
-    }
-  },
-  async updateUser(req: Request, res: Response) {
-    try {
-      const { body, params } = req
-      const user = await services.updateUser(body, params)
-      response.isOk(res, 200, 'Usuario actualizado correctamente', { user })
-    } catch (error) {
-      response.isError(res, 500, error as CustomError, 'user')
-    }
-  },
-  async deleteUser(req: Request, res: Response) {
-    try {
-      const { params } = req
-      await services.deleteUser(params)
-      response.isOk(res, 200, 'Usuario eliminado correctamente')
-    } catch (error) {
-      response.isError(res, 500, error as CustomError, 'user')
-    }
-  },
-  async recoveryPassword(req: Request, res: Response) {
-    try {
-      await services.recoveryPassword(req.body)
-      response.isOk(res, 200, 'Correo enviado correctamente')
-    } catch (error) {
-      response.isError(res, 500, error as CustomError, 'user')
-    }
-  },
-  async signIn(req: Request, res: Response) {
-    try {
-      const user = await services.signIn(req.body)
-      response.isOk(res, 200, 'Usuario logueado correctamente', user)
-    } catch (error) {
-      response.isError(res, 500, error as CustomError, 'user')
-    }
-  },
-  async signInToken(req: Request, res: Response) {
-    try {
-      //@ts-ignore
-      const resp = await services.signInToken(req.user)
-      const data = {
-        token: resp.token,
-        user: resp.user,
-      }
-      response.isOk(res, 200, 'Token generado correctamente', data)
-    } catch (error) {
-      response.isError(res, 500, error as CustomError, 'user')
-    }
-  },
+    async createUser(req: Request, res: Response) {
+        try {
+            await services.createUser(req.body)
+            response.isOk(res, 201, 'Usuario creado correctamente')
+        } catch (error) {
+            response.isError(res, 500, error as CustomError, 'user')
+        }
+    },
+    async updateUser(req: Request, res: Response) {
+        try {
+            const { body, params } = req
+            const user = await services.updateUser(body, params)
+            response.isOk(res, 200, 'Usuario actualizado correctamente', {
+                user,
+            })
+        } catch (error) {
+            response.isError(res, 500, error as CustomError, 'user')
+        }
+    },
+    async deleteUser(req: Request, res: Response) {
+        try {
+            const { params } = req
+            await services.deleteUser(params)
+            response.isOk(res, 200, 'Usuario eliminado correctamente')
+        } catch (error) {
+            response.isError(res, 500, error as CustomError, 'user')
+        }
+    },
+    async recoveryPassword(req: Request, res: Response) {
+        try {
+            await services.recoveryPassword(req.body)
+            response.isOk(res, 200, 'Correo enviado correctamente')
+        } catch (error) {
+            response.isError(res, 500, error as CustomError, 'user')
+        }
+    },
+    async signIn(req: Request, res: Response) {
+        try {
+            const user = await services.signIn(req.body)
+            response.isOk(res, 200, 'Usuario logueado correctamente', user)
+        } catch (error) {
+            response.isError(res, 500, error as CustomError, 'user')
+        }
+    },
+    async signInToken(req: Request, res: Response) {
+        try {
+            //@ts-ignore
+            const resp = await services.signInToken(req.user)
+            const data = {
+                token: resp.token,
+                user: resp.userToken,
+            }
+            response.isOk(res, 200, 'Token recuperado correctamente', data)
+        } catch (error) {
+            response.isError(res, 500, error as CustomError, 'user')
+        }
+    },
 }
--- src/middlewares/passport.ts
+++ src/middlewares/passport.ts
@@ -3,26 +3,28 @@ import passportJwt from 'passport-jwt'
 import User from '../models/user'
 
 passport.use(
-  new passportJwt.Strategy(
-    {
-      jwtFromRequest: passportJwt.ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: <string>process.env.SECRET,
-    },
-    async (jwtPayload, done) => {
-      try {
-        console.log('pase por el middleware')
-        const user = await User.findOne({ _id: jwtPayload.id as string })
-        if (user) {
-          return done(null, user)
-        }
-        console.log('no encontre el usuario')
-        return done(null, false)
-      } catch (error) {
-        console.log(error)
-        return done(error, false)
-      }
-    },
-  ),
+    new passportJwt.Strategy(
+        {
+            jwtFromRequest:
+                passportJwt.ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey: <string>process.env.SECRET,
+        },
+        async (jwtPayload, done) => {
+            try {
+                const user = await User.findOne({
+                    _id: jwtPayload.id as string,
+                })
+                if (user) {
+                    return done(null, user)
+                }
+                console.log('no encontre el usuario')
+                return done(null, false)
+            } catch (error) {
+                console.log(error)
+                return done(error, false)
+            }
+        },
+    ),
 )
 
 export default passport
